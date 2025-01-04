8f72d19648300c8b744ea80994c39b1c10508fc3
Rash499 <rashmikadilmin499@gmail.com>
2025-01-04 15:59:19 +0530

    add animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,52 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants';
 import porfilePic from "../assets/kevinRushProfile.png"
+import {motion} from "framer-motion"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {duaration: 0.5, delay: delay}
+    }
+})
 const Hero = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16'>Rashmika Dilmith</h1>
+                    <motion.h1 
+                    variants={container(0)}
+                    initial="hidden"
+                    animate="visible"
+                    className='pb-16 text-6xl font-thin tracking-tight lg:mt-16'>Rashmika Dilmith</motion.h1>
 
-                    <sapn className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                    <motion.span
+                    variants={container(0.5)}
+                    initial="hidden"
+                    animate="visible"
+                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                         Web Development, Automation and Linux Kernel Development.
-                    </sapn>
+                    </motion.span>
 
-                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+                    <motion.p 
+                    variants={container(1)}
+                    initial="hidden"
+                    animate="visible"
+                    className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                        {HERO_CONTENT} 
-                    </p>
+                    </motion.p>
                 </div>
             </div>
             <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                    <img src={porfilePic} alt="propic" />
+                    <motion.img 
+                    initial={{x:100,opacity:0}}
+                    animate={{x:0, opacity:1}}
+                    transition={{duaration:1, delay:1.2}}
+                    src={porfilePic} alt="propic" />
                 </div>
             </div>
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,89 +11,187 @@ import { FaHtml5 } from "react-icons/fa";
 import { FcLinux } from "react-icons/fc";
 import { SiCplusplus, SiTailwindcss, SiExpress, SiScala } from "react-icons/si";
 import { FaGitAlt } from "react-icons/fa6";
+import {motion} from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: {y:-10},
+    animate: {
+        y: [10,-10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
 
 const Technologies = () => {
   return (
-    <div className='border-b border-neutral-800 pb-24'>
+    <motion.div
+    whileInView={{opacity:1, y:0}}
+    initial={{opacity: 0, y:-100}}
+    transition={{duaration: 1.5}}
+     className='border-b border-neutral-800 pb-24'>
         <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className='flex flex-wrap item-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.div 
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity: 0, x:-100}}
+        transition={{duaration: 1.5}}
+        className='flex flex-wrap item-center justify-center gap-4'>
+            <motion.div
+            variants={iconVariants(1.5)}
+            initial="initial"
+            animate="animate"
+             className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMongodb className='text-7xl text-green-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaJava className='text-7xl text-red-700'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div
+            variants={iconVariants(3.5)}
+            initial="initial"
+            animate="animate"
+             className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaGitAlt className='text-7xl text-orange-700'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiDotnet className='text-7xl'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(1.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaNodeJs className='text-7xl text-green-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaPhp className='text-7xl text-indigo-600'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaPython className='text-7xl text-blue-600'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandCSharp className='text-7xl text-purple-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(3.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiCss3 className='text-7xl text-blue-600'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMysql className='text-7xl text-blue-600'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiGnubash className='text-7xl'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaHtml5 className='text-7xl text-orange-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(3.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiJavascript className='text-7xl text-yellow-300'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FcLinux className='text-7xl text-cyan-400'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(1.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiCplusplus className='text-7xl text-blue-600'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiExpress className='text-7xl text-gray-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiTailwindcss className='text-7xl text-teal-500'/>
-            </div>
+            </motion.div>
 
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiScala className='text-7xl text-red-500'/>
-            </div>
-        </div>
-    </div>
+            </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
 
